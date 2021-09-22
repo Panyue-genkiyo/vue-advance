cf35ed67f63a0226150b1ee79c7577b65eaa538f
panyue <90938238g@gmail.com>
2021-09-22 16:57:46 +0800

    vue脚手架默认配置文件

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+//common js 暴露
+module.exports = {
+    pages: {
+        index: {
+            //入口
+            entry: 'src/main.js',
+        },
+    },
+    //关掉默认的语法检查
+    lintOnSave: false
+}
