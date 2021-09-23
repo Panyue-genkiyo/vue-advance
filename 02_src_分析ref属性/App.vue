a9b99cd02b62d6eea1a8b3f87bbab7682e992485
panyue <90938238g@gmail.com>
2021-09-23 15:24:41 +0800

    分析ref属性

---
+++ 02_src_分析ref属性/App.vue
@@ -0,0 +1,31 @@
+<template>
+   <div>
+     <h1 v-text="msg" ref="title"></h1>
+     <button @click="showH">点我输出上方的dom元素</button>
+     <School ref="sch"/>
+   </div>
+</template>
+
+<script>
+import School from "./components/School";
+
+export default {
+  name: "App",
+  data(){
+    return {
+      msg: '欢迎学习Vue',
+    }
+  },
+  methods:{
+    showH(e){
+      console.log(this.$refs.title); //this ==> vc(app组件)
+      console.log(e.target); //发生事件的dom元素m
+      console.log(this.$refs.sch); //可以是school组件加refs属性 获得的是组件事例对象vc
+    }
+  },
+  components:{
+    School,
+  },
+}
+</script>
+
---
+++ 02_src_分析ref属性/components/School.vue
@@ -0,0 +1,23 @@
+<template>
+   <div class="demo">
+     <h1>学校名称:{{ name }}</h1>
+     <h1>学校地址: {{  address }}</h1>
+   </div>
+</template>
+
+<script>
+export default {
+  name: "School",
+  data(){
+    return {
+      address: '武汉',
+      name: '武汉科技大学'
+    }
+  }
+}
+</script>
+<style scoped>
+   .demo{
+     background: aqua;
+   }
+</style>
