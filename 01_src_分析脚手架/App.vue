094087cf11cb61b6ec5193a0c337eaba55ffb015
panyue <90938238g@gmail.com>
2021-09-23 15:22:35 +0800

    分析vue脚手架的文件的作用

---
+++ 01_src_分析脚手架/App.vue
@@ -0,0 +1,22 @@
+<template>
+   <div>
+     <img src="./assets/logo.png" alt="logo">
+     <School></School>
+     <Student></Student>
+   </div>
+</template>
+
+<script>
+import School from './components/School';
+import Student from "./components/Student";
+
+export default {
+  name: "App",
+  //汇总所有的组件
+  components:{
+    Student,
+    School
+  }
+}
+</script>
+
