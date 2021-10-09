86ca3f13c29b5129bb4780f20acaa1d86608d45e
panyue <90938238g@gmail.com>
2021-10-10 00:28:01 +0800

    vue中的代理配置

---
+++ 19_src_vue中配置代理(解决跨域请求的问题)/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button @click="getStudents">获取学生信息</button>
+    <button @click="getCars">获取汽车信息</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: "App",
+  methods:{
+     getStudents(){
+       axios.get('/api/students') //其实这里就是请求http://localhost:8080/students只不过把请求转移给了端口5001
+       .then(res => console.log(res.data))
+       .catch(e => console.log(`msg: ${e.message}, stack: ${e.stack}`));
+    },
+    getCars(){
+      axios.get('/demo/cars') //同理
+          .then(res => console.log(res.data))
+          .catch(e => console.log(`msg: ${e.message}, stack: ${e.stack}`));
+    }
+  }
+}
+</script>
+
+
+
