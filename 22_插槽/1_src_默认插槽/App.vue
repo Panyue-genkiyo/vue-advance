bad7318814d8dbed63221d8f3639ccf1750123eb
panyue <90938238g@gmail.com>
2021-10-10 15:30:12 +0800

    vue中的默认插槽

---
+++ 22_插槽/1_src_默认插槽/App.vue
@@ -0,0 +1,49 @@
+<template>
+   <div class="container">
+     <Category title="美食">
+       <img src="" alt="delicious food"/>
+     </Category>
+
+     <Category title="游戏" :listData="games">
+       <ul>
+         <li v-for="(g , index) in games" :key="index">{{ g }}</li>
+       </ul>
+     </Category>
+
+     <Category title="电影">
+       <video src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" controls></video>
+     </Category>
+   </div>
+</template>
+
+<script>
+import Category from "@/components/Category";
+export default {
+  name: "App",
+  components:{
+    Category
+  },
+  data(){
+    return {
+      foods:['火锅','烧烤','小龙虾','牛排'],
+      games:['红色警戒','穿越火线','劲舞团','超级玛丽'],
+      films:['《教父》','《拆弹专家》','《你好，李焕英》','《尚硅谷》']
+    }
+  }
+}
+</script>
+<style lang="css" scoped>
+   .container{
+     display: flex;
+     justify-content: space-around;
+   }
+   video {
+     width: 100%;
+   }
+   img{
+     width: 100%;
+   }
+</style>
+
+
+
---
+++ src/components/Category.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="category">
+    <h3>{{ title }}</h3>
+    <!--插槽,等着组件的使用者进行填充-->
+    <slot>我是默认值</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Category",
+  props:[ 'listData', 'title' ]
+}
+</script>
+
+<style scoped>
+   .category{
+     background: skyblue;
+     width: 200px;
+     height: 300px;
+   }
+   h3{
+     text-align: center;
+     background: orange;
+   }
+   img{
+     width: 100%;
+   }
+</style>
