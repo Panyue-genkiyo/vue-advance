2b18453f1348b24c6e6aca419a4a2cecf050de69
panyue <90938238g@gmail.com>
2021-10-10 16:16:41 +0800

    vue中的作用域插槽

---
+++ 22_插槽/3_src_作用域插槽/App.vue
@@ -0,0 +1,46 @@
+<template>
+   <div class="container">
+     <Category title="游戏">
+       <template scope="{games}">
+         <!--这里data为插槽给你传递的对象包含你所需要的数据 包装成对象的原因就是你当时可能个插槽传递了多个数据-->
+         <ul>
+           <li v-for="(g , index) in games" :key="index">{{ g }}</li>
+         </ul>
+       </template>
+     </Category>
+
+     <Category title="游戏">
+       <template scope="{games}">
+         <!--这里data为插槽给你传递的对象包含你所需要的数据-->
+         <ol>
+           <li  v-for="(g , index) in games" :key="index">{{ g }}</li>
+         </ol>
+       </template>
+     </Category>
+
+     <Category title="游戏">
+       <template slot-scope="{games}">
+         <h4 v-for="(g , index) in games" :key="index">{{ g }}</h4>
+       </template>
+     </Category>
+   </div>
+</template>
+
+<script>
+import Category from "@/components/Category";
+export default {
+  name: "App",
+  components:{
+    Category
+  },
+}
+</script>
+<style lang="css" scoped>
+   .container, .foot{
+     display: flex;
+     justify-content: space-around;
+   }
+</style>
+
+
+
---
+++ 22_插槽/3_src_作用域插槽/components/Category.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="category">
+    <h3>{{ title }}</h3>
+    <!--插槽,等着组件的使用者进行填充-->
+    <slot :games="games">
+      我是默认值
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Category",
+  props:[ 'listData', 'title' ],
+  data(){
+    return {
+      games:['红色警戒','穿越火线','劲舞团','超级玛丽'],
+    }
+  }
+}
+</script>
+
+<style scoped>
+   .category{
+     background: skyblue;
+     width: 200px;
+     height: 300px;
+   }
+   h3{
+     text-align: center;
+     background: orange;
+   }
+</style>
