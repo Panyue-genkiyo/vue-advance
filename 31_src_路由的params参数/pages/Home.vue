26f8e58e92c5d3538eda97c6947be6bdecbf6261
panyue <90938238g@gmail.com>
2021-10-11 23:46:14 +0800

    路由中利用params传递参数

---
+++ 31_src_路由的params参数/pages/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2>我是Home的内容</h2>
+    <div>
+      <ul class="nav nav-tabs">
+        <li>
+          <router-link class="list-group-item" active-class="active" to="/home/news">News</router-link>
+        </li>
+        <li>
+          <router-link class="list-group-item" active-class="active" to="/home/message">Message</router-link>
+        </li>
+      </ul>
+      <router-view>
+      </router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  // mounted() {
+  //   console.log('Home组件挂载完毕', this);
+  //   window.homeRoute = this.$route;
+  //   window.homeRouter = this.$router;
+  // },
+  // // beforeDestroy() {
+  // //   console.log('Home组件将要被销毁');
+  // // }
+}
+</script>
+
+<style scoped>
+</style>
