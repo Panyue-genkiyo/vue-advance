28b60c5126be68e5a2fb318253acb2860dc68737
panyue <90938238g@gmail.com>
2021-10-11 22:42:45 +0800

    vue路由传递query参数

---
+++ 29_路由的query参数/router/index.js
@@ -0,0 +1,40 @@
+//该文件专门用于创建整个应用的路由器
+
+import VueRouter from "vue-router";
+import About from "@/pages/About";
+import Home from '@/pages/Home';
+import News from "@/pages/News";
+import Message from "@/pages/Message";
+import * as path from "path";
+import Detail from "@/pages/Detail";
+
+//创建并默认暴露一个路由器
+export default new VueRouter({
+   routes:[
+       {
+           path:'/about',
+           component: About
+       },
+       {
+           path:'/home',
+           component: Home,
+           children:[
+               {
+                   path: 'news',
+                   component: News
+               },
+               {
+                   path: 'message',
+                   component: Message,
+                   children:[
+                       {
+                           path: 'detail',
+                           component: Detail
+                       }
+                   ],
+               }
+           ]
+       }
+   ]
+});
+
