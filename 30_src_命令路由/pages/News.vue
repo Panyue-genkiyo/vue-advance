bd5e6e09bc8659a8aee01b8dd6d840081c231cd9
panyue <90938238g@gmail.com>
2021-10-11 23:22:02 +0800

    vue中给路由命名,简化路由配置

---
+++ 30_src_命令路由/pages/News.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li>news001</li>
+    <li>news002</li>
+    <li>news003</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "News"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ 30_src_命令路由/router/index.js
@@ -0,0 +1,41 @@
+//该文件专门用于创建整个应用的路由器
+
+import VueRouter from "vue-router";
+import About from "@/pages/About";
+import Home from '@/pages/Home';
+import News from "@/pages/News";
+import Message from "@/pages/Message";
+import Detail from "@/pages/Detail";
+
+//创建并默认暴露一个路由器
+export default new VueRouter({
+   routes:[
+       {
+           name: 'regard',
+           path:'/about',
+           component: About
+       },
+       {
+           path:'/home',
+           component: Home,
+           children:[
+               {
+                   path: 'news',
+                   component: News
+               },
+               {
+                   path: 'message',
+                   component: Message,
+                   children:[
+                       {
+                           name: 'particulars',
+                           path: 'detail',
+                           component: Detail
+                       }
+                   ],
+               }
+           ]
+       }
+   ]
+});
+
