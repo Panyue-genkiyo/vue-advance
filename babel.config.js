2934607c83ce6662c89f2f9752b25cd25e7caf99
panyue <90938238g@gmail.com>
2021-10-12 15:45:50 +0800

    添加element-ui按需引入的配置信息(参考官网)

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    '@vue/cli-plugin-babel/preset',
+    ["@babel/preset-env", { "modules": false }],
+  ],
+  plugins: [
+    [
+      "component",
+      {
+        "libraryName": "element-ui",
+        "styleLibraryName": "theme-chalk"
+      }
+    ]
   ]
 }
