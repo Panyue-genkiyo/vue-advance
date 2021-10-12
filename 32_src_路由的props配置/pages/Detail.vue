478327ac6f724426e1a833c44e45bc58b6131113
panyue <90938238g@gmail.com>
2021-10-12 10:22:17 +0800

    路由中使用props配置简化组件获取params,query参数

---
+++ 32_src_路由的props配置/pages/Detail.vue
@@ -0,0 +1,20 @@
+<template>
+   <ul>
+     <li>消息编号:{{ id }}</li>
+     <li>消息标题:{{ title }}</li>
+   </ul>
+</template>
+
+<script>
+export default {
+  name: "Detail",
+  props: ['id', 'title']
+  // mounted() {
+  //   console.log(this.$route);
+  // }
+}
+</script>
+
+<style scoped>
+
+</style>
