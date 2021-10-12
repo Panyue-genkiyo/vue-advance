72b65cea1086ee74693c60441411c20f6ff45fee
panyue <90938238g@gmail.com>
2021-10-12 15:06:24 +0800

    vue路由history和hash两种模式

---
+++ 40_src_history模式和hash模式/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="row">
+      <Banner/>
+    </div>
+    <div class="row">
+      <div class="col-xs-2 col-xs-offset-2">
+        <div class="list-group">
+          <!--原始使用a标签跳转多个页面,多页面应用-->
+<!--          <a class="list-group-item active" href="./about.html">About</a>-->
+<!--          <a class="list-group-item" href="./home.html">Home</a>-->
+          <!--vue中借助router=link标签实现路由的切换-->
+<!--          <router-link class="list-group-item" active-class="active" to="/about">About</router-link>-->
+          <router-link class="list-group-item" active-class="active" :to="{name: 'regard'}">About</router-link>
+          <router-link class="list-group-item" active-class="active" to="/home">Home</router-link>
+        </div>
+      </div>
+      <div class="col-xs-6">
+        <div class="panel">
+          <div class="panel-body">
+            <!--router-view确定视图的位置-->
+            <router-view>
+            </router-view>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Banner from "@/components/Banner";
+export default {
+  name: "App",
+  components: {Banner},
+}
+</script>
+<style lang="css" scoped>
+
+</style>
+
+
+
---
+++ 40_src_history模式和hash模式/pages/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2>我是Home的内容</h2>
+    <div>
+      <ul class="nav nav-tabs">
+        <li>
+          <router-link class="list-group-item" active-class="active" to="/home/news">News</router-link>
+        </li>
+        <li>
+          <router-link class="list-group-item" active-class="active" to="/home/message">Message</router-link>
+        </li>
+      </ul>
+      <!--include的值代表要缓存的组件，比如下面代表在Home组件中要缓存News组件(组件名)-->
+      <keep-alive include="News">
+        <router-view>
+        </router-view>
+      </keep-alive>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  // mounted() {
+  //   console.log('Home组件挂载完毕', this);
+  //   window.homeRoute = this.$route;
+  //   window.homeRouter = this.$router;
+  // },
+  // // beforeDestroy() {
+  // //   console.log('Home组件将要被销毁');
+  // // }
+}
+</script>
+
+<style scoped>
+</style>
